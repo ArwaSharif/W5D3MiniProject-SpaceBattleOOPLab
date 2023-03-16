812fdedee24338e3fd92374ba8f49f89bffbf075
Arwa <arwamsharif@gmail.com>
2023-03-16 16:30:56 -0500

    starting the project

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+- make a game factory to have:
+    - the rounds of battle 
+    - the attacks 
+    - end of the game
+
+- Make a ship factory to:
+    - produce USS Per Scholas AND 6 Alien Ships
+    - create a hull, firepower, accruacy
+    - give USSPS: hull - 20, firepower - 5, accuracy - .7.
+    - give each of the 6 AlienShips unique: hull - between 3and 6, firepower - between 2and 4, accuracy - between .6and .8
+
+*/
+
